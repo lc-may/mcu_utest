4edef3631904cbf63cb0a0c5ed97a1ac4479b768
rfhuang <rfhuang@marvell.com>
2014-07-24 19:11:33 +0800

    update readline.py

--- readline.py
+++ readline.py
@@ -2,7 +2,7 @@
 import re
  
 for i,line in enumerate(sys.stdin): 
-		pattern = re.compile(r'^[0-9]{8,16} T (mu_\w+$)')
+		pattern = re.compile(r'^[0-9a-f]{8,16} T _?(mu_\w+$)')
 		match = pattern.match(line);
 		if match:
 			sys.stdout.write(match.group(1) + '\n')
